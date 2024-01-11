f6038e30e20d6ec7ed8c2aa3d78e296578813dae
lalita agarwal <lalitaagarwal2211@gmail.com>
2024-01-11 22:57:39 +0530

    complex number

---
+++ datatypes/complex_dayatype.py
@@ -0,0 +1,23 @@
+
+#declare complex number
+x = 10 + 12j
+print(x)
+
+#declare complex number with floating point
+y = 10.2 + 12.1j
+print(y)
+
+#get the real number of complex number
+a = 10+2j
+print(a.real)
+#get the imaginary number of complex number
+print(a.imag)
+
+#get addition of complex number
+print(x+y)
+
+#declare complex number with binary
+b = 0b1001 + 12.1j
+print(b)
+
+
