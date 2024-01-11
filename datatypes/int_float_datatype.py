2a584be5a0b1cf71e446620393a85e4ba9e68bfb
lalita agarwal <lalitaagarwal2211@gmail.com>
2024-01-11 16:10:45 +0530

    int and float type

---
+++ datatypes/int_float_datatype.py
@@ -0,0 +1,18 @@
+#int data type
+a = 123
+print(a)
+print(type(a))
+print(id(a))
+
+#float datatype
+f = 1.234
+print(f)
+print(type(f))
+
+#floating value in exponential form
+fl = 1.23e3
+print(fl)
+print(type(fl))
+
+
+
