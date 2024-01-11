686ce0a980e53cfeabec57b3108a23a2edb9938e
lalita agarwal <lalitaagarwal2211@gmail.com>
2024-01-11 23:20:02 +0530

    bool datatype

---
+++ datatypes/bool_datatype.py
@@ -0,0 +1,33 @@
+#declare bool type variable
+a = True
+b = False
+
+print(a)
+print(b)
+
+#get the type of above var
+
+print(type(a))
+print(type(b))
+
+x=10
+y=20
+z=x<y
+print(x)
+print(y)
+print(z)
+
+print(type(z))
+
+#airthmetic addition with bool type
+a1 = True + True
+b1 = True - False
+c1 = a1+b1
+
+print(a1)
+print(b1)
+print(c1)
+
+print(type(a1))
+
+
