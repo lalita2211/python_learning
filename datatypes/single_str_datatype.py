f0c4c94ffa8676106ab78b82738bb0837be77c23
lalita agarwal <lalitaagarwal2211@gmail.com>
2024-01-12 14:00:46 +0530

    single double triple quote str

---
+++ datatypes/single_str_datatype.py
@@ -0,0 +1,38 @@
+#Define single quote
+s = 'I will be good programer'
+print("s=", s)
+print("type of s:", type(s))
+
+#Define double quote
+s1 = "I will be good programer"
+print("s1=", s1)
+print("type of s1:", type(s1))
+
+#In python single char treated as a str datatype
+c = 'a'
+print(c)
+print(type(c))
+
+#Uses of triple quote
+#for multiline string we use triple quote
+
+str = '''I
+      will
+      be
+      good
+      programer'''
+print("str=", str)
+print("type of str:", type(str))
+
+#use single and double both letter with string use of triple quote
+
+str2 = """I will be good "programer" & will placed in a good 'company'"""
+print("str2=", str2)
+
+#use single quote letter with string
+str3 = "I will be good 'programer'"
+print("str3=", str3)
+
+#use double quote letter with string
+str4 = 'I will be placed in a good "company"'
+print("str4=", str4)
