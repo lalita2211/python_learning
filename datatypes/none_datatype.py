34e7d676e8633776e1a0d261e1e185f834a9fb0c
lalita agarwal <lalitaagarwal2211@gmail.com>
2024-01-18 22:50:25 +0530

    None datatype

---
+++ datatypes/none_datatype.py
@@ -0,0 +1,19 @@
+#novalue associate
+
+a = None
+b = None
+c = None
+
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+d = None
+print(d)
+print(type(d))
+print(id(d))
